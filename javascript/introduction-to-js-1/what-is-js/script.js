c71ad362a6e08d2b4a0010dd5f02888865297059
Ben <bzilber@users.noreply.github.com>
2021-11-15 13:46:06 -0500

    Playing with code

--- javascript/introduction-to-js-1/what-is-js/script.js
+++ javascript/introduction-to-js-1/what-is-js/script.js
@@ -6,6 +6,6 @@ function createParagraph() {
 
 const buttons = document.querySelectorAll('button');
 
-for(let i = 0; i < buttons.length ; i++) {
+for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', createParagraph);
 }
